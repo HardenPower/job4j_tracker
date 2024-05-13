0bdf7bcded115a7134e89a5076c828d541fbaee0
HardenPower <a.boot96@gmail.com>
2024-05-13 23:46:34 +0300

    Revert "1.4. Вызов метода с возвращаемым типом. [#167387]"
    This reverts commit 15e0b21d79d3d9b3b4f7c2d677bc9dfce86422a1.

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,31 +1,8 @@
 package ru.job4j.oop;
 
 public class Cat {
-
-    private String food;
-    private String name;
-
-    public void show() {
-        System.out.println("Cat's name is: " + this.name);
-        System.out.println("Cat's food is: " + this.food);
-    }
-
-    public void giveNick(String nick) {
-        this.name = nick;
-    }
-
-    public void eat(String meat) {
-        this.food = meat;
-    }
-
     public static void main(String[] args) {
-        Cat gav = new Cat();
-        gav.giveNick("gav");
-        gav.eat("kotleta");
-        gav.show();
-        Cat black = new Cat();
-        black.giveNick("black");
-        black.eat("fish");
-        black.show();
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
     }
 }
