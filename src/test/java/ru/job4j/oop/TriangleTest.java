4f0cce7976a452d432653018595ddeba6fad7587
HardenPower <a.boot96@gmail.com>
2024-06-17 19:23:48 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -26,4 +26,44 @@ public class TriangleTest {
         double expected = -1;
         assertThat(result).isCloseTo(expected, offset(0.001));
     }
+
+    @Test
+    public void when000and01010and0410Then10Dot77() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 10, 10);
+        Point c = new Point(0, 4, 10);
+        double result = c.distance3d(a);
+        double expected = 10.77;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when000and01010and0410Then14Dot1() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 10, 10);
+        Point c = new Point(0, 4, 10);
+        double result = b.distance3d(a);
+        double expected = 14.1;
+        assertThat(result).isCloseTo(expected, offset(0.1));
+    }
+
+    @Test
+    public void when000and01010and0410Then6() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 10, 10);
+        Point c = new Point(0, 4, 10);
+        double result = c.distance3d(b);
+        double expected = 6;
+        assertThat(result).isCloseTo(expected, offset(0.1));
+    }
+
+    @Test
+    public void when000and01010and0410Then10Dot18() {
+        Point a = new Point(0, 0, 20);
+        Point b = new Point(0, 15, 10);
+        Point c = new Point(6, 7, 8);
+        double result = b.distance3d(c);
+        double expected = 10.18;
+        assertThat(result).isCloseTo(expected, offset(0.1));
+    }
 }
