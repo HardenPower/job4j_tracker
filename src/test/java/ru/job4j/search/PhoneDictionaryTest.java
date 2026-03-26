3103a07064054fc3fd080c6673062de48a77f809
HardenPower <a.boot96@gmail.com>
2026-03-26 22:12:56 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Arsentev");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("534872");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Bryansk");
+        assertThat(persons.get(0).getAddress()).isEqualTo("Bryansk");
+    }
+
+    @Test
+    public void whenFindNothing() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("EmptyUser");
+        assertThat(persons).isEmpty();
+    }
+}
