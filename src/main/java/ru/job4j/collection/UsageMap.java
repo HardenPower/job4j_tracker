1a692b2c8a4a7e3c05f351365e232616e121b2b4
HardenPower <a.boot96@gmail.com>
2026-04-02 15:38:14 +0300

    0. Map, HashMap [#212652 #561645]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("j4j@ya.ru", "John Malcolm Doe");
+        map.put("admin@ya.ru", "Admin Admin Admin");
+        map.put("admin@vk.ru", "VK CEO");
+        map.put("admin@vk.ru", "VK CTO");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " = " + entry.getValue());
         }
