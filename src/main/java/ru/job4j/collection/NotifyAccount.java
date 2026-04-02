4bed98f6da25c146eb45931196e2cbf2b9cc6645
HardenPower <a.boot96@gmail.com>
2026-04-02 15:34:54 +0300

    3. Модели данных и HashSet. [#218530 #561616]

--- src/main/java/ru/job4j/collection/NotifyAccount.java
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class NotifyAccount {
     public static HashSet<Account> sent(List<Account> accounts) {
         HashSet<Account> result = new HashSet<>();
-        for (Account elem: accounts) {
+        for (Account elem : accounts) {
             result.add(elem);
         }
         return result;
