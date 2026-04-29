40369d3237cab2365b389c7d74215b895ce17289
HardenPower <a.boot96@gmail.com>
2026-04-29 08:07:01 +0300

    3. Optional - empty, of [#214711 #563132]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -13,31 +13,17 @@ public class College {
     }
 
     public Optional<Student> findByAccount(String account) {
-        Student stud = students.keySet()
+        return students.keySet()
                 .stream()
                 .filter(student -> student.account().equals(account))
-                .findFirst()
-                .orElse(null);
-        if (stud == null) {
-            return Optional.empty();
-        }
-        return Optional.of(stud);
+                .findFirst();
     }
 
     public Optional<Subject> findBySubjectName(String account, String name) {
         Optional<Student> studentOptional = findByAccount(account);
-        if (!studentOptional.isPresent()) {
-            return Optional.empty();
-        }
-        Student student = studentOptional.get();
-        Subject subjects =  students.get(student)
-                    .stream()
-                    .filter(subject -> subject.name().equals(name))
-                    .findFirst()
-                    .orElse(null);
-        if (subjects == null) {
-           return Optional.empty();
-        }
-        return Optional.of(subjects);
+        return studentOptional.flatMap(student -> students.get(student)
+                .stream()
+                .filter(subject -> subject.name().equals(name))
+                .findFirst());
     }
 }
